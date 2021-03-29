a44e9d8fa765c7ca90c61f305fb386680d156bb1
Wahyu Ramadhan <ssjrama@gmail.com>
2021-03-29 12:35:13 +0700

    Coba git

--- cuci-sepatu/resources/views/welcome.blade.php
+++ cuci-sepatu/resources/views/welcome.blade.php
@@ -117,7 +117,7 @@
                             </svg>
 
                             <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                                Sponsor
+                                Laravel Sponsor
                             </a>
                         </div>
                     </div>
