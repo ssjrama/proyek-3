19925fc901fa3e839d3701317407695f06955051
Wahyu Ramadhan <ssjrama@gmail.com>
2021-04-03 10:10:25 +0700

    Adding web controller, Adding datatables plugin, Modify login redirect, Blade layout

--- cuci-sepatu/routes/web.php
+++ cuci-sepatu/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\TreatmentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->group(function (){
+    Route::get('/dashboard', function () {return view('dashboard');});
+    
+    Route::resources([
+        'treatment' => TreatmentController::class,
+        'order' => OrderController::class,
+    ]);
+});
