9b5ed90ffeaf5646de8664286e54727a061c7b67
Wahyu Ramadhan <ssjrama@gmail.com>
2021-06-08 07:35:50 +0700

    Small Fix

--- cuci-sepatu/resources/views/admin/order/edit.blade.php
+++ cuci-sepatu/resources/views/admin/order/edit.blade.php
@@ -14,8 +14,16 @@
             </div>
             @endforeach
             <div class="form-group">
+                <label for="item_name">Nama / Merk sepatu</label>
+                <input type="text" name="item_name" class="form-control" value="{{$order->item_name}}">
+            </div>
+            <div class="form-group">
                 <label for="address">Alamat</label>
                 <textarea name="address" rows="5" class="form-control">{{$order->address}}</textarea>
+            </div>
+            <div class="form-group">
+                <label for="description">Deskripsi</label>
+                <textarea name="description" rows="5" class="form-control">{{$order->description}}</textarea>
             </div>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
